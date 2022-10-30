ef1bdde3de017d2750a2df3e736bf39a91683fb8
Ilpo Lehtinen <ilpo.lehtinen@solita.fi>
2022-10-30 14:06:55 +0200

    Initial Commit

---
+++ New Murja Client/New_Murja_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Murja_ClientApp.swift
+//  New Murja Client
+//
+//  Created by Ilpo Lehtinen on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct New_Murja_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
